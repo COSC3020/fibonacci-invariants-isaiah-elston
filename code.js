a83ac4bcf5c3cdee5eeed2b3e063adc1a72b9db1
Isaiah Elston <156853632+isaiah-elston@users.noreply.github.com>
2024-02-08 20:27:33 +0000

    Testing that implementation passes tests

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
 function fib(n) {
-    return n;
+    if (n <= 0) {
+        return [0];
+    } else if (n == 1) {
+        return [0, 1];
+    } else if (n > 1) {
+        return fib(n - 1).concat([fib(n - 2)[n - 2] + fib(n - 1)[n - 1]]);
+    }
 }
